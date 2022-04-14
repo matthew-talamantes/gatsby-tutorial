03c7e9e0d840c555785f7560afa203a3deea9f88
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-04-14 17:23:47 -0400

    Continued Tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <Layout>
+      <StaticImage
+        src="../assets/images/main.jpeg"
+        alt="Eggs and flour"
+        placeholder="tracedSVG"
+        layout="fullWidth"
+      />
       <h1>Home</h1>
     </Layout>
   )
