6ebe974902acb53e51e1119a9cf52538a1e30ef2
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-04-19 21:23:24 -0400

    Continue work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,41 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
-      <h1>About Us</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Asymmetrical jean shorts 3 wolf moon</h2>
+            <p>
+              Stumptown readymade cold-pressed jean shorts. Twee distillery palo
+              santo green juice air plant selfies mustache banh mi narwhal.
+              Typewriter jianbing cardigan hot chicken pork belly, letterpress
+              waistcoat squid organic small batch pok pok helvetica.
+            </p>
+            <p>
+              Flexitarian hell of cray blue bottle locavore salvia readymade
+              cliche selfies lomo umami. Woke vape raw denim, leggings tbh air
+              plant gastropub cred salvia iceland. Kickstarter shoreditch beard,
+              art party four loko brunch occupy iceland mustache marfa actually.
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/images"
 
 export default function Home() {
   return (
     <Layout>
-      <Images />
       <h1>Home</h1>
     </Layout>
   )
